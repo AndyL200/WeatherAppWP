cfa3c09c2ad1b8b40d08e3e89d22d04c2cf0ea24
Andrew <aleclair.1105@outlook.com>
2024-07-22 16:10:21 -0500

    functionality fix and adding vue

---
+++ project/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <homePage msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import homePage from './components/homePage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    homePage
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
